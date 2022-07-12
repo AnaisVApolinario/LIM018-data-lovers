eed70d29b25e8cb0221ca048845c225fb56e3b54
VanezaCT <cryse.835@gmail.com>
2022-07-12 09:46:21 -0500

    filtrocss

--- src/data.js
+++ src/data.js
@@ -10,20 +10,6 @@ export const anotherExample = () => {
 export const pociones_ordenadas = ()=> {
   console.log(data.potions.sort((a,b)=>a.name>b.name?-1:1));
 } 
-
-
-
-/*export const sortPociones = (data,order)=> {
-  let newPotions=[...data];
-  let arrOrder=newPotions.sort((a,b)=>a.name<b.name?-order:order);
-  return arrOrder
-  data.potions.sort((a,b)=>{return (a.name > b.name)?-1:1 });
-} 
-export const sortData = (data, order) => {
-  //Creando un nuevo array de data para ordenarlo sin modificar el original
-  let newArrPokemon = [...data];
-
-  //Ordenando los pokemon alfabéticamente
-  let arrOrder = newArrPokemon.sort((a, b) => a.name < b.name ? -order : order); //-order a se va a posicionar antes (asc)
-  return arrOrder;
-};*/
+ export const filtrarPersonajes =()=>{
+  
+ }
