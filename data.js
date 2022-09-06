cdd8b56dd9ba63ea1e14139d6244fe0a61442702
Anais <avalverdea54@gmail.com>
2022-09-06 17:39:45 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,35 @@
+
+export const pociones_ordenadas = (dataPocion,num1,num2)=> {
+  const newPotions=[...dataPocion];
+  const arrOrder=newPotions.sort((a,b)=>a.name<b.name?num1:num2);
+  return arrOrder;
+} 
+ export const filtrarPersonajes =(characters,opcion,valorOption)=>{ 
+  const newPersonajes=[...characters]
+  const personajesFiltrado=newPersonajes.filter((el)=>{
+    return  el[opcion] === valorOption;
+  })
+  return personajesFiltrado;
+ }
+
+
+export const filtroHechi=(dataSpell,nHechizo)=>{
+  const newHechizos=[...dataSpell]
+  const hechizoFiltrado=newHechizos.filter((el)=>{
+  return el.spell_type=== nHechizo;
+});
+return hechizoFiltrado;
+}
+export const computeStats=(dataPersonajes,FoM)=>{
+  const newData=[...dataPersonajes];
+  // contar cuantas veces nos encontramos un elemento que cumpla la caracteristica 
+  let contador=0;
+  // recorremos todos los elementos buscando los que tengan la caracteristica 
+  newData.forEach((element)=>{
+    if(element.gender===FoM)
+    {
+      contador += 1;
+    }
+  });
+  return contador; 
+}
